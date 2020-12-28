f79525b34bee82249526c65e6d084a071be0609c
baramatsubonzo <baramatsubonzo@gmail.com>
2020-12-28 11:13:21 +0900

    クイズ画面で財務情報を出力できるところまで終えた

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,7 +1,9 @@
 class QuizzesController < ApplicationController
   def index
+    @accounting_information = AccountingInformation.all.sample
   end
 
   def show
+    # @accounting_information = AccountingInformation.find params[:id]
   end
 end
