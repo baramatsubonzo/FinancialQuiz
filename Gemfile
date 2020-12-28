70ba7d6f9828f98de404ba309ed0964635223868
baramatsubonzo <baramatsubonzo@gmail.com>
2020-12-28 13:00:16 +0900

    chartkickを導入し、円グラフで、資産の部/負債の部を表示できるようにした

--- Gemfile
+++ Gemfile
@@ -34,6 +34,8 @@ gem 'devise'
 
 gem 'active_hash'
 
+gem "chartkick"
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,6 +1,8 @@
 class QuizzesController < ApplicationController
   def index
     @accounting_information = AccountingInformation.all.sample
+    @asset_data = { '流動資産' => @accounting_information.current_asset, '固定資産' => @accounting_information.fixed_asset }
+    @liability_data = { '流動負債' => @accounting_information.current_liability, '固定負債' => @accounting_information.fixed_liability, '純資産' => @accounting_information.net_asset }
   end
 
   def show
